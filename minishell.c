6944d51460192d88d52c67ce0ddf45c455f80260
Mohammed Radouani <mradouan@e1r2p3.1337.ma>
2025-04-20 13:32:55 +0100

    mini

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mradouan <mradouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 14:21:56 by mradouan          #+#    #+#             */
-/*   Updated: 2025/04/17 15:02:45 by mradouan         ###   ########.fr       */
+/*   Updated: 2025/04/17 23:51:23 by mradouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,22 +61,22 @@ char **fetch_path(t_env *my_env)
 	return (NULL);
 }
 
-// void print_node(t_env *my_env)
-// {
-// 	while (my_env)
-// 	{
-// 		printf("%s = %s\n", my_env->key, my_env->value);
-// 		my_env = my_env->next;
-// 	}
-// }
-// void print_node2(t_list *my_env)
-// {
-	// 	while (my_env)
-	// 	{
-		// 		printf("%s -> [%d]\n", my_env->data, my_env->type);
-		// 		my_env = my_env->next;
-		// 	}
-		// }
+void print_node(t_env *my_env)
+{
+	while (my_env)
+	{
+		printf("%s = %s\n", my_env->key, my_env->value);
+		my_env = my_env->next;
+	}
+}
+void print_node2(t_list *my_env)
+{
+	while (my_env)
+	{
+		printf("%s -> [%d]\n", my_env->data, my_env->type);
+		my_env = my_env->next;
+	}
+}
 char *is_accessable(char **path, char *cmd)
 {
 	int i;
@@ -203,7 +203,6 @@ char	**loop_through_node(t_list *nodes, char **cmd)
 		}
 		head = head->next;
 	}
-	// redirection(nodes);
 	cmd = helper_loop(cmd, nodes);
 	return (cmd);
 }
@@ -247,28 +246,67 @@ char **each_group_cmd(t_list *nodes)
 	return (full_group_cmd);
 }
 
-int	piping_forking(char **cmd_path, char **full_cmd, char **envirment, int num_pipes)
+int help_split_node(t_list *nodes)
+{
+	t_list	*head;
+	int 	num_groups;
+
+	num_groups = 1;
+	head = nodes;
+	while (head)
+	{
+		if (head->type == 5)
+			num_groups++;
+		head = head->next;	
+	}
+	head = nodes;
+	return (num_groups);
+}
+
+t_list	**split_nodes_by_pipe(t_list *nodes, int *num_groups)
+{
+	t_list 	**groups;
+	t_list	*group;
+	int 	i;
+	t_list	*head;
+
+	i = 0;
+	*num_groups = help_split_node(nodes);
+	groups = malloc((*num_groups + 1) * sizeof(t_list *));
+	group = NULL;
+	head = nodes;
+	if (!groups)
+		return (NULL);
+	while (head)
+	{
+		if (head->type == 5)
+		{
+			groups[i++] = group;
+			group = NULL;
+		}
+		else
+			ft_lstadd_backk(&group, ft_lstneww(head->data, head->type));
+		head = head->next;
+	}
+	groups[i] = group;
+	groups[i + 1] = NULL;
+	return (groups);
+}
+
+int	piping_forking(char *cmd_path, char **cmd, char **group_cmd, t_list *nodes, t_env *my_env)
 {
 	int pip_fd[2];
-	t_list *cmd_start;
-	t_list *cmd_end;
 	int prev_fd = -1;
+	t_list **groups;
+	int num_groups;
+	char **cmd2;
 	int i = 0;
 	pid_t	id;
 	
-	// pip_fd = malloc(num_pipes * sizeof(int));
-	// if (!pip_fd)
-	// 		return (NULL);
-	// while (num_pipes > i)
-	// {
-	// 	pip_fd[i] = malloc(2 * sizeof(int));
-	// 	i++;
-	// 	if (!pip_fd[i])
-	// 		return (md_free(pip_fd), NULL);
-	// }
-	while (num_pipes)
+	groups = split_nodes_by_pipe(nodes, &num_groups);
+	while (i < num_groups)
 	{
-		if (pipe(pip_fd[i] == -1))
+		if (pipe(pip_fd) == -1)
 			return (-1);
 		id = fork();
 		if (id == -1)
@@ -280,13 +318,17 @@ int	piping_forking(char **cmd_path, char **full_cmd, char **envirment, int num_p
 				dup2(prev_fd, STDIN_FILENO);
 				close(prev_fd);
 			}
-			if (cmd_end)
-			{
+			if (i + 1 < num_groups)
 				dup2(pip_fd[1], STDOUT_FILENO);
-				close(pip_fd[0]);
-				close(pip_fd[1]);
-			}
-			
+			close(pip_fd[1]);
+			close(pip_fd[0]);
+			cmd = loop_through_node(groups[i], NULL);
+			cmd_path = is_accessable(fetch_path(my_env), cmd[0]);
+			if (!cmd_path)
+				exit(write(2, "mhd: command not found\n", 24));
+			execve(cmd_path, cmd, load_env(my_env));
+			perror("execeve (cmd2)");
+			exit(1);
 		}
 		else
 		{
@@ -294,56 +336,43 @@ int	piping_forking(char **cmd_path, char **full_cmd, char **envirment, int num_p
 				close(prev_fd);
 			close(pip_fd[1]);
 			prev_fd = pip_fd[0];
-			if (cmd_end)
-				cmd_start = cmd_end->next;
-			else
-				break ;
+			i++;
 		}
-		wait(NULL);
 	}
+	while (wait(NULL) > 0);
+	return (0);
 }
 
 void exec_commands(t_list *nodes, t_env *my_env)
 {
-	char **path;
 	char *cmd_path;
 	char **cmd;
-	char **envirment;
-	char **full_cmd;
+	// char **full_cmd;
 	char **group_cmd;
-	int id;
-	int num_pipes;
 
 	ft_lstadd_backk(&nodes, ft_lstneww("cat", 0));
-	// ft_lstadd_backk(&nodes, ft_lstneww("lst_functions.c", 0));
-	// ft_lstadd_backk(&nodes, ft_lstneww("mari", 2));
-	// ft_lstadd_backk(&nodes, ft_lstneww("maro", 2));
 	ft_lstadd_backk(&nodes, ft_lstneww("minishell.h", 0));
-	ft_lstadd_backk(&nodes, ft_lstneww("cat", 2));
+	ft_lstadd_backk(&nodes, ft_lstneww("mini", 2));
 	ft_lstadd_backk(&nodes, ft_lstneww("|", 5));
-	ft_lstadd_backk(&nodes, ft_lstneww("moha", 0));
-	ft_lstadd_backk(&nodes, ft_lstneww("uy", 0));
-	cmd = loop_through_node(nodes, cmd);
+	ft_lstadd_backk(&nodes, ft_lstneww("grep", 0));
+	ft_lstadd_backk(&nodes, ft_lstneww("int", 0));
+	ft_lstadd_backk(&nodes, ft_lstneww("|", 5));
+	ft_lstadd_backk(&nodes, ft_lstneww("wc", 0));
+	ft_lstadd_backk(&nodes, ft_lstneww("-w", 0));
+
 	if (!cmd)
 	{
 		perror("Minishell");
 		exit(1);
-	}
-	path = fetch_path(my_env);
-	cmd_path = is_accessable(path, cmd[0]);
-	if (!cmd_path)
-	{
-		write(2, "mhd: command not found\n", 24);
-		exit(1);
-	}
-	full_cmd = create_full_cmd(nodes);
-	envirment = load_env(my_env);
+	}	
+	// full_cmd = create_full_cmd(nodes);
 	group_cmd = each_group_cmd(nodes);
-	num_pipes = count_pipes(nodes);
-	piping_forking(cmd_path, full_cmd, envirment, num_pipes);
-	execve(cmd_path, full_cmd, envirment);
-	perror("execeve (cmd2)");
-	exit(1);
+	if (piping_forking(cmd_path, cmd, group_cmd, nodes, my_env) == -1)
+	{
+		perror("Minishell");
+		exit(1);
+	}	
+	return ;	
 }
 void	claiming_env(char **env_p, t_env *my_env)
 {
